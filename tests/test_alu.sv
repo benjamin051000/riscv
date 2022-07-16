222e5cb4e171638e507ffbfb4830f172976ec692
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-07-16 02:20:32 -0400

    simple testbench with one test (not working on local modelsim... try server)

---
+++ tests/test_alu.sv
@@ -0,0 +1,29 @@
+`timescale 1ns/10ps
+
+import ALU_FN::*;
+
+module test_alu;
+
+initial begin
+    localparam WIDTH = 32;
+
+    logic [2:0] fn;
+    logic [31:25] funct7; // For Integer Register-Register Operations
+    logic [WIDTH-1:0] a;
+    logic [WIDTH-1:0] b;
+    logic [WIDTH-1:0] out;
+
+    alu #(.WIDTH(WIDTH)) DUT (.*);
+
+    initial begin
+        fn = AND;
+        funct7 = '0;
+        a = 23;
+        b = 11;
+        #10;
+        $display("Done.");
+    end
+    
+end
+
+endmodule
