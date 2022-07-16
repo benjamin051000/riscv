37c994fc9276c761b2e0ef4dd09295a1245ae874
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-07-16 14:25:02 +0000

    fixed tb issues, it compiles now

--- tests/test_alu.sv
+++ tests/test_alu.sv
@@ -4,26 +4,23 @@ import ALU_FN::*;
 
 module test_alu;
 
+localparam WIDTH = 32;
+
+logic [2:0] fn;
+logic [31:25] funct7; // For Integer Register-Register Operations
+logic [WIDTH-1:0] a;
+logic [WIDTH-1:0] b;
+logic [WIDTH-1:0] out;
+
+alu #(.WIDTH(WIDTH)) DUT (.*);
+
 initial begin
-    localparam WIDTH = 32;
-
-    logic [2:0] fn;
-    logic [31:25] funct7; // For Integer Register-Register Operations
-    logic [WIDTH-1:0] a;
-    logic [WIDTH-1:0] b;
-    logic [WIDTH-1:0] out;
-
-    alu #(.WIDTH(WIDTH)) DUT (.*);
-
-    initial begin
-        fn = AND;
-        funct7 = '0;
-        a = 23;
-        b = 11;
-        #10;
-        $display("Done.");
-    end
-    
+    fn = AND;
+    funct7 = '0;
+    a = 23;
+    b = 11;
+    #10;
+    $display("Done.");
 end
 
 endmodule
