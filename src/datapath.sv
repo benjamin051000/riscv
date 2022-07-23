b045fc04e9e8561ec946fd7262ac658d04873015
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-07-23 16:57:35 -0400

    added alu_inb as net (bugfix). Removed IMM mux: focos on R-Type first.

--- src/datapath.sv
+++ src/datapath.sv
@@ -6,7 +6,7 @@ module datapath #(
     input logic clk, rst, sel
 );
 
-logic [WIDTH-1:0] addr_a, addr_b, wr_addr;
+logic [WIDTH-1:0] addr_a, addr_b, wr_addr, alu_inb;
 logic wr_en;
 logic [WIDTH-1:0] a, b, wr_data;
 alu_fn_t fn;
@@ -32,7 +32,8 @@ register #(.WIDTH(WIDTH)) _ir (.d(instruction), .*);
 
 regfile #(.WIDTH(WIDTH)) _regfile (.*);
 
-assign alu_inb = sel ? b : q;
+// assign alu_inb = sel ? b : q;
+assign alu_inb = b;
 
 alu #(.WIDTH(WIDTH)) _alu (.b(alu_inb), .*);
 
