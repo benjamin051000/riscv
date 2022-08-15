b3e971ad34abb624a13faa38979a9cbcfa321675
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-08-15 03:18:32 +0000

    much improved tests. register.sv passes this tb

--- tests/test_register.sv
+++ tests/test_register.sv
@@ -5,30 +5,40 @@ module test_register;
 localparam NUM_TESTS = 1000;
 localparam WIDTH = 32;
 
-logic clk = 1'b0, rst, en;
+logic clk, rst = 1'b1, en = 1'b0;
 logic[WIDTH-1:0] d, q;
 
 register #(.WIDTH(WIDTH)) DUT (.*);
 
-always begin : generate_clk
-    #5 clk = ~clk;
+initial begin : generate_clk
+    clk = 1'b0;
+    while(1) #5 clk = ~clk;
 end
 
 initial begin : drive_inputs
     $timeformat(-9, 0, "ns");
 
+    rst <= 1'b1;
+    for(int i = 0; i < 5; i++) @(posedge clk);
+    rst <= 1'b0;
+
     for(int i = 0; i < NUM_TESTS; i++) begin
-        rst <= $random;
-        in <= $random;
+        d <= $random;
         en <= $random;
         @(posedge clk);
     end
-    $display("Done.");
 
     disable generate_clk;
+    $display("Done.");
 end
 
-assert property (@(posedge clk) disable iff (rst) en |=> q == $past(d, 1));
-assert property (@(posedge clk) disable iff (rst) !en |=> $stable(q));
+assert property (@(posedge clk) disable iff (rst) en |=> q == $past(d, 1))
+    else $error("d not saved when enabled.");
+
+assert property (@(posedge clk) disable iff (rst) !en |=> $stable(q))
+    else $error("q not stable when not enabled.");
+
+always @(rst) assert (q == '0)
+    else $error("q not 0 when reset.");
 
 endmodule
